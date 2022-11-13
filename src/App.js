73e35e0853377fe644f8f6bb52c56a01a0a8ed6c
swh_95 <tjdngur22@gmail.com>
2022-11-13 19:57:37 +0900

    FIX(clenUp react-study)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useState, useEffect } from "react";
+
+function Hello() {
+  useEffect(() => {
+    console.log("hi");
+    return () => console.log("bye");
+  }, []);
+  return <h1>Hello</h1>;
+}
+
 function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <h1>Welcome Back!</h1>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
 
-export default App
+export default App;
