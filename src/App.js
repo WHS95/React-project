7f321e3572c861163e3432dd9f9f296b8a562e7f
swh_95 <tjdngur22@gmail.com>
2022-11-13 19:02:48 +0900

    FIX(useEffect react -study)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+// import Button from "./Button";
+// import styles from "./App.module.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [value, setValue] = useState(0);
+  const onClick = () => setCounter((data) => data + 1);
+  const onChange = (event) => setValue(event.target.value);
+  console.log("i run all time");
+  useEffect(() => {
+    console.log("i run only one");
+  }, []);
+  useEffect(() => {
+    if (value !== "" && value.length > 4) console.log("search for", value);
+  }, [value]);
   return (
     <div>
-      <h1 className={styles.title}>Welcome Back!</h1>
-      <Button text={"Continue"} />
+      <input
+        value={value}
+        onChange={onChange}
+        type='string'
+        palceholder='Search here'
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
