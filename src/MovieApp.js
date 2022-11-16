d801f145e817d4e52528c0cba4c0346aae4d9fe6
swh_95 <tjdngur22@gmail.com>
2022-11-17 00:09:31 +0900

    FIX(Movie.js: BrowserRouter basename add))
    BrowserRouter basename 을 통해 github 위치 반영

--- src/MovieApp.js
+++ src/MovieApp.js
@@ -1,17 +1,17 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Router, Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 import ErrorPage from "./routes/Error";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/' errorElement={<ErrorPage />} />
-        <Route path='/movie' element={<Detail />} />
-      </Routes>
-    </Router>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Routes>
+          <Route path={"/"} element={<Home />} />
+          <Route path={"/"} errorElement={<ErrorPage />} />
+          <Route path={"/movie/:id"} element={<Detail />} />
+        </Routes>
+    </BrowserRouter>
   );
 }
 export default App;
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,7 +1,24 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 function Detail() {
-    return (
-        <h1>Detail</h1>
+  const { id } = useParams();
+  const [movie, setMovie] = useState([]);
+  const [loading, setloading] = useState(false);
+  const getMovieDetailData = async () => {
+    const response = await fetch(
+      `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`
     );
-  }
-  export default Detail;
-  
+    const json = await response.json();
+    setMovie(json.data.movie);
+    setloading(false);
+  };
+  useEffect(() => {
+    getMovieDetailData();
+  }, []);
+  console.log(id);
+  console.log(movie);
+  return <div>{loading ? <h1>Loading</h1> : null}</div>;
+}
+
+export default Detail;
